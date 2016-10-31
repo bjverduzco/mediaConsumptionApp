c2fa4eea621a49f3ab7800acf09be3d1b7362e64
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-10-30 22:09:21 -0500

    login controller

--- public/scripts/LoginController.js
+++ public/scripts/LoginController.js
@@ -1,3 +1,20 @@
 angular.module('mediaApp').controller('LoginController', ['$http', '$location', function($http, $location){
   var vm = this;
+
+  vm.username = '';
+  vm.password = '';
+
+  vm login = function(){
+    var sendData = {};
+
+    sendData.username = vm.username;
+    sendData.password = vm.password;
+
+    $http.post('/login', sendData).then(function(response){
+        vm.userData.user = response.data;
+        console.log(vm.userData.user);
+    }, function(err){
+      console.log('Error in userservice', err);
+    })
+  }
 }]);
