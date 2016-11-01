9545be67946a40c796d75442e297cfe08d0277a5
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-10-31 23:15:35 -0500

    userservice

---
+++ public/scripts/UserService.js
@@ -0,0 +1,18 @@
+angular.module('mediaApp').factory('UserService', ['$http', function($http){
+  var vm = this;
+
+  vmuserData = {};
+
+  function login(data){
+    return $http.post('/login', data).then(function(response){
+      vm.userData.user = response.data;
+      console.log('User Data', vm.userData.user);
+    }, function(err){
+      console.log('Error in UserService', err);
+    });
+  };
+
+  return {
+    login: login;
+  }
+}]);
